b018303adf56eb8c6acb02315e64a1b7fcfd491d
Axel Cotón Gutiérrez <133801080+AxelCotonGutierrez@users.noreply.github.com>
2023-11-17 13:03:52 +0100

    Ordinales

---
+++ README.md
@@ -0,0 +1,24 @@
+# Juego para aprender los números ordinales
+
+
+![Juego de los Números Ordinales](https://axelcotongutierrez.github.io/learningmathematicas/assets/images//posts/05JSND/J02SND.jpg)
+
+Dentro del blog: https://axelcotongutierrez.github.io/learningmathematicas/
+
+Vídeo Explicativo: https://www.youtube
+
+Juego para Ingfantil, Primaria, ESO, ESPA o ESPAD para aprender los Ordinales
+
+Puedes probar el juego en esta url:
+
+https://github.com/AxelCotonGutierrez/Juego-de-las-Cifras
+
+¡Prepárate para una emocionante aventura de aprendizaje en nuestro juego sobre el Sistema de Numeración Decimal!
+
+En este desafío, tendrás la oportunidad de poner a prueba y mejorar tu dominio sobre el sistema numérico que utilizamos en la vida cotidiana. Se te harán una serie de preguntas donde tendrás que responder con precisión a la pregunta ¿Cómo se dice ... en números ordinales?
+
+Nuestro juego está diseñado para adaptarse a tu nivel de habilidad. Hay cuatro niveles de 1-10, de 1-25, de 1-50 y de 1-100.
+
+Lo mejor de todo es que no hay límite en la cantidad de preguntas que puedes responder. Juega tanto como desees, hasta que te sientas completamente satisfecho o hasta que domines el nivel que necesitas.
+
+Así que, ¿estás listo para sumergirte en este desafío y convertirte en un experto en este juego. ¡Diviértete explorando, aprendiendo y superando tus propios límites! ¡El mundo de los números te espera!
---
+++ script.js
@@ -0,0 +1,140 @@
+// Axel Cotón Gutiérrez Copyright 2023
+let currentQuestionIndex = 0;
+
+// Variables para gestionar la dificultad y el rango
+let difficulty = "easy"; // Valor predeterminado
+let maxNumber = 10;
+
+// Escucha el evento de cambio en los radios de selección de dificultad
+const difficultyButtons = document.getElementsByName("difficulty");
+for (const difficultyButton of difficultyButtons) {
+    difficultyButton.addEventListener("change", function () {
+        difficulty = this.value;
+        if (difficulty === "easy") {
+            maxNumber = 10;
+        } else if (difficulty === "medium") {
+            maxNumber = 25;
+        } else if (difficulty === "high") {
+            maxNumber = 50;
+        } else if (difficulty === "vhigh") {
+            maxNumber = 100;
+        }
+        displayQuestion();
+    });
+}
+
+// Función para generar una pregunta en función de la dificultad
+function generateQuestion() {
+    const number = Math.floor(Math.random() * maxNumber) + 1;
+    const question = `¿Cómo se dice ${number}.º en números ordinales?`;
+    const answer = toOrdinal(number);
+
+    return {
+        question: question,
+        options: generateOptions(answer),
+        answer: answer
+    };
+}
+
+function generateOptions(answer) {
+    const options = [answer];
+    while (options.length < 4) {
+        const randomOption = generateRandomOrdinal();
+        if (!options.includes(randomOption)) {
+            options.push(randomOption);
+        }
+    }
+    return shuffleArray(options);
+}
+
+function generateRandomOrdinal() {
+    const randomNum = Math.floor(Math.random() * maxNumber) + 1;
+    return toOrdinal(randomNum);
+}
+
+
+function toOrdinal(number) {
+    if (number === 1) {
+        return 'primero';
+    } else if (number === 2) {
+        return 'segundo';
+    } else if (number === 3) {
+        return 'tercero';
+    } else if (number === 4) {
+        return 'cuarto';
+    } else if (number === 5) {
+        return 'quinto';
+    } else if (number === 6) {
+        return 'sexto';
+    } else if (number === 7) {
+        return 'séptimo';
+    } else if (number === 8) {
+        return 'octavo';
+    } else if (number === 9) {
+        return 'noveno';
+    } else {
+        const tens = Math.floor(number / 10) * 10;
+        const units = number % 10;
+
+        const tensName = {
+            10: 'décimo',
+            20: 'vigésimo',
+            30: 'trigésimo',
+            40: 'cuadragésimo',
+            50: 'quincuagésimo',
+            60: 'sexagésimo',
+            70: 'septuagésimo',
+            80: 'octogésimo',
+            90: 'nonagésimo'
+        }[tens];
+
+        const unitsName = units > 0 ? ' ' + toOrdinal(units) : '';
+
+        return tensName + unitsName;
+    }
+}
+
+function shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+function displayQuestion() {
+    const currentQuestion = generateQuestion();
+    document.getElementById("question").textContent = currentQuestion.question;
+
+    const optionsContainer = document.getElementById("options-container");
+    optionsContainer.innerHTML = "";
+
+    currentQuestion.options.forEach((option, index) => {
+        const button = document.createElement("button");
+        button.textContent = option;
+        button.classList.add("options-container"); // Agrega la clase aquí
+        button.addEventListener("click", () => checkAnswer(option, currentQuestion.answer));
+        optionsContainer.appendChild(button);
+    });
+}
+
+function checkAnswer(selectedOption, correctAnswer) {
+    const resultContainer = document.getElementById("result");
+
+    if (selectedOption === correctAnswer) {
+        resultContainer.textContent = "¡Respuesta Correcta!";
+        resultContainer.style.color = "green";
+    } else {
+        resultContainer.textContent = "Respuesta Incorrecta. La respuesta correcta es " + correctAnswer + ".";
+        resultContainer.style.color = "red";
+    }
+
+    // Resto del código...
+}
+
+function nextQuestion() {
+    const resultContainer = document.getElementById("result");
+    resultContainer.textContent = ""; // Limpiar el mensaje de resultado al pasar a la siguiente pregunta
+
+    displayQuestion();
+}
+
+
+// Inicia el juego al cargar la página
+window.onload = displayQuestion;
